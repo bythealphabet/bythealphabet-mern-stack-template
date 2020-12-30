2d992dc86f17e097a5e72d0179730aca66da88fd
bythealphabet <bythealphabet@gmail.com>
2020-12-30 12:22:42 -0400

    adding package.json file, webpack.config and babel.config files to project with install packages

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+function webpackConfig() {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
+      ],
+    },
+  };
+}
+
+module.exports = webpackConfig;
